1187354f2b1fc100ad3058399aa9e2c7aee81c94
Dev <140449517+Moho4Maya@users.noreply.github.com>
2024-01-28 11:08:04 +0000

    Learn about basic routes in express.js

--- nodeBasicApp/index.js
+++ nodeBasicApp/index.js
@@ -1,1 +1,26 @@
-console.log("Hello Dev!!!");
+require('dotenv').config()
+const express = require('express');
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send("Hello Divyansh!!!")
+})
+
+app.get('/twitter', (req, res)=> {
+    res.send("devdotcom")
+})
+
+app.get('/login', (req, res)=>{
+    res.send("<h1> please login at chai aur code </h1>")
+})
+
+app.get('/chai', (req, res)=>{
+    res.send("<h2>Chai aur Code</h2>")
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example app listening on port ${port}`);
+})
